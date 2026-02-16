339a9e18a4f91c2aab804ef541fa4ebdb2659009
Vitor Barcellos <vtor.barcellos7@gmail.com>
2026-02-16 20:42:41 -0300

    Rename ConsolePong

--- _1_Entities/Match/AssemblyInfo.cs
+++ _1_Entities/Match/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("EntitiesTests")]
-[assembly: InternalsVisibleTo("ConsoleMain")]
+[assembly: InternalsVisibleTo("ConsolePong")]
--- _2_Use_Cases/PlayersInteractor/AssemblyInfo.cs
+++ _2_Use_Cases/PlayersInteractor/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("ConsoleMain")]
+[assembly: InternalsVisibleTo("ConsolePong")]
